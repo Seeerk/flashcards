465a0182b878ba7eb3cbc2551fcece16821fbc98
Seeerk <edik.serkov@gmail.com>
2021-05-10 01:22:49 +0500

    Initilization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      id: 0,
+      word: "",
+      translate: "",
+      cards: []
+    }
+  }
+
+  updateInput(key, value) {
+    this.setState({
+      [key]: value
+    })
+  }
+
+  addCard() {
+    const newCard = {
+      id: 1 + this.state.id,
+      value: {
+        word: this.state.word.slice(),
+        translate: this.state.translate.slice(),
+        overturned: false
+      }
+    }
+
+    this.setState({
+      id: newCard.id,
+      word: "",
+      translate: "",
+      cards: [...this.state.cards, newCard]
+    })
+  }
+
+  turnCard(id) {
+    const cards = [...this.state.cards]
+
+    let index = cards.findIndex((card) => {
+      return card.id === id
+    })
+
+    cards[index].value.overturned = !cards[index].value.overturned;
+
+    this.updateInput("cards", cards)
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <h1 className="app-title">FlashCards</h1>
+          <div className="container">
+            <h2>Add Card</h2>
+            <input
+                type="text"
+                placeholder="Enter learning word"
+                value={this.state.word}
+                onChange={(e) => this.updateInput('word', e.target.value)}
+            />
+            <input
+                type="text"
+                placeholder="Enter translate"
+                value={this.state.translate}
+                onChange={(e) => this.updateInput('translate', e.target.value)}
+            />
+            <button
+                className="btn add-btn"
+                onClick={() => this.addCard()}
+            >
+              add
+            </button>
+            <div>
+              {this.state.cards.map(card => {
+                return(
+                    <div
+                      key={card.id}
+                      className={'card' + (card.value.overturned ? "overturned" : "")}
+                      onClick={() => this.turnCard(card.id)}
+                    >
+                      {card.value.overturned ? card.value.translate : card.value.word}
+                    </div>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+    )
+  }
 }
 
-export default App;
+export default App
